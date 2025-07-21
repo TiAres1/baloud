edbd5b3c9862aaaf8d93abe9cebf02f49c9faf5b
Mazen <mezn4010100@gmail.com>
2025-07-21 07:53:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,7 +68,7 @@ client.on('error', (error) => {
     console.error('❌ خطأ في البوت:', error);
 });
 
-client.login(process.env.DISCORD_TOKEN).catch(error => {
+client.login("MTM1ODc3NDA5MzM1NjQ2NjIxNw.Gs8ZQh.ECUBw3Lo6UJZrA6emiMMUppdOcp5ymT4lt1PmU").catch(error => {
     console.error('❌ فشل في تسجيل دخول البوت:', error);
     console.log('تأكد من أن token البوت صحيح في ملف .env');
 });
